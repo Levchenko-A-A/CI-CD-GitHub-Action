2f76a26d002d5124821d2f7a0bfa3dadbcb1982b
Levchenkov Andrei <lev_a_a@mail.ru>
2025-11-23 11:23:12 -0800

    Setup webpack

--- src/runApp.js
+++ src/runApp.js
@@ -1,22 +1,34 @@
+import './runApp.css';
+
 export function runApp(el) {
     const input = document.createElement('input');
-  Object.assign(input, {
-    className: 'text-field',
-    type: 'text',
-    placeholder: 'Введите текст',
-  });
-  
-  const button = document.createElement('button');
-  Object.assign(button, {
-    className: 'button',
-    textContent: 'Нажми меня',
-    disabled: true,
-  });
+    Object.assign(input, {
+        className: 'text-field',
+        type: 'text',
+        placeholder: 'Введите текст',
+    });
 
-  createAndAppendElement('p', 'First Paragraph', el);
-  createAndAppendElement('p', 'Second Paragraph', el);
-  createAndAppendElement('p', 'Third Paragraph', el);
+    const button = document.createElement('button');
+    Object.assign(button, {
+        className: 'button',
+        textContent: 'Нажми меня',
+        //disabled: true,
+    });
 
-  el.append(input);
-  el.append(button);
+    el.append(input);
+    el.append(button);
+
+    createAndAppendElement('p', 'First Paragraph', el);
+    createAndAppendElement('p', 'Second Paragraph', el);
+    createAndAppendElement('p', 'Third Paragraph', el);
+
+    /**
+   * Creates HTML element, set its text content and appends it to
+   * the specified parent element
+   */
+    function createAndAppendElement(tag, text, parent) {
+        const element = document.createElement(tag);
+        element.textContent = text;
+        parent.appendChild(element);
+    }
 }
